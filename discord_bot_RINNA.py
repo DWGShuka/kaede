cf2722da39126594c420a0c8529fc5e02add1f90
DWGShuka <k14i12d35@gmail.com>
2020-07-01 20:33:12 +0900

    commit

--- discord_bot_RINNA.py
+++ discord_bot_RINNA.py
@@ -1,10 +1,10 @@
 import discord
 import asyncio
-import datetime
 from discord.ext import tasks
+from login_chousei import chousei
+import datetime
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
     loop.start()
@@ -12,22 +12,22 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-n_time=datetime.datetime.now()
+@client.event
+async def on_message(message):
+    n_time = datetime.datetime.now()
+    if message.content.startswith('今何時？'):
+        await message.channel.send(n_time.strftime('%H:%M'))
 @tasks.loop(seconds=60)
 async def loop():
+    channel_id = client.get_channel(721738331968241752)
     if not client.is_ready():
         return
     c_time="22:50"
+    n_time = datetime.datetime.now()
+    if n_time.weekday()==2:
+        if n_time.strftime('%H:%M')==c_time:
+            await channel_id.send(chousei())
     if n_time.weekday()==1:
         if n_time.strftime('%H:%M')==c_time:
-            channel_id = client.get_channel(721738331968241752)
             await channel_id.send('みんな会議だよ\tしゅーごー')
-#    if message.content.startswith('今何時？'):
-#        await client.send_message(message.channel, n_time.time())
-@client.event
-async def on_message(message):
-    if message.content.startswith('今何時？'):
-        await client.send_message(message.channel, n_time.time())
-
 client.run('NDY2NDAyODQyNTAwOTIzMzky.DimuMQ.zWnIzerHztwo_rHnqRhvqB68GdE')
--- login_chousei.py
+++ login_chousei.py
@@ -1,11 +1,16 @@
-from robobrowser import robobrowser
-import sqlite3
+from robobrowser import RoboBrowser
+import datetime
 
-browser = RoboBrowser(parser='html.parser')
-browser.open('https://chouseisan.com/schedule/newEvent/create')
-
-form = browser.get_form(attrs={'id':'newEventForm'})
-form['name']='絶アレキサンダー'
-form['comment']='今週もよろしくお願いします'
-form['kouho']= a '\n' b '\n' c '\n' d '\n' e '\n' f '\n' g
-browser.submit_form(form,headers={'Referer':browser.url,'Accept-Language':'ja,en-US;q=0.7,en;q=0.3',})
+def chousei():
+    thisweek=[datetime.date.today()+datetime.timedelta(days = i) for i in range(7)]
+    strweek=[i.strftime('%Y/%m/%d') for i in thisweek]
+    browser = RoboBrowser(parser='html.parser')
+    browser.open('https://chouseisan.com/schedule/newEvent/create')
+    form = browser.get_form(attrs={'id':'newEventForm'})
+    form['name']='絶アレキサンダー'
+    form['comment']='今週もよろしくお願いします'
+    form['kouho']= strweek[0] +'\n'+ strweek[1]+ '\n'+ strweek[2]+ '\n'+ strweek[3]+ '\n'+ strweek[4]+ '\n'+ strweek[5] +'\n'+ strweek[6]
+    browser.submit_form(form,headers={'Referer':browser.url,'Accept-Language':'ja,en-US;q=0.7,en;q=0.3',})
+    print('on processing')
+    print(browser.find(attrs={'id':'listUrl'}))
+    return browser.find(attrs={'id':'listUrl'})
